5b9da6bd926585977151b3497fdcdfa1c929b9d5
lirfu <jurajfulir@gmail.com>
2018-05-14 10:00:04 +0200

    Run tests problem by problem

--- TestEnv/tester.sh
+++ TestEnv/tester.sh
@@ -2,13 +2,15 @@
 
 mkdir logs 2> /dev/null
 
-for run in {1..5}
+for f in tests/*
 do
-    for f in tests/*
+    echo "Running $f ..."
+    for run in {1..7}
     do
-        echo "Running $f ..."
+        echo "Starting run $run/7"
         ././../cmake-build-debug/NeuroEvolution $f > "logs/$(basename $f)_$run.log" &
     done
 
     wait
+    echo "Done!"
 done
